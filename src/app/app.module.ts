ed26530df6394ba77798c62e1651c485a831811a
solenn <solenn.provost@campus.academy>
2022-06-27 16:31:06 +0200

    mise en place du calendrier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CalendarCommonModule} from "angular-calendar";
+import {DemoModule} from "./components/calendar/demo.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    DemoModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalendarCommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/components/calendar/demo.component.ts
@@ -0,0 +1,45 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+} from '@angular/core';
+import { CalendarEvent, CalendarView, DAYS_OF_WEEK } from 'angular-calendar';
+import { colors } from '../calendar-utils/colors';
+
+@Component({
+  selector: 'mwl-demo-component',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: 'template.html',
+})
+export class DemoComponent {
+  view: CalendarView = CalendarView.Month;
+
+  viewDate: Date = new Date();
+
+  dayStartHour: number = 8;
+  dayEndHour: number = 19;
+
+  events: CalendarEvent[] = [
+    {
+      start: new Date('2022-06-23'),
+      end: new Date('2022-06-30'),
+      title: 'One day excluded event',
+      color: colors.red,
+      allDay: true,
+    },
+    {
+      start: new Date('2022-06-23'),
+      end: new Date('2022-06-30'),
+      title: 'Multiple weeks event',
+      allDay: true,
+    },
+  ];
+
+  // exclude weekends
+  excludeDays: number[] = [0, 6];
+
+  weekStartsOn = DAYS_OF_WEEK.SUNDAY;
+
+  CalendarView = CalendarView;
+}
