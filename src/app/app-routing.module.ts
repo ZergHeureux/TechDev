e0b8f8b91ae235eaf28af3ea52764b279ea4f62e
solenn <solenn.provost@campus.academy>
2022-06-27 16:39:39 +0200

    container calendar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DemoComponent} from "./components/calendar/demo.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'calendar', component:DemoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
