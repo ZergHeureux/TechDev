7a05da271775b8d777ab87bf7957ba097d43e6a9
ErwanLeflot <erwan.leflot@gmail.com>
2022-06-27 16:10:40 +0200

    basic manifest operationnel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ApplicationRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TechDev';
+  constructor(private applicationRef: ApplicationRef){}
+  
+  ngOnInit() {
+    this.applicationRef.isStable.subscribe((t) => {
+      console.log('App stable: ' + t);
+    });
+  }
 }
