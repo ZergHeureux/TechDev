173589e8b23daa0e87fb3f58f6636e2a89cf314a
solenn <solenn.provost@campus.academy>
2022-06-28 10:58:30 +0200

    fix template and button for view days

--- src/app/components/calendar-view/calendar-view-all/calendar-view-all.component.ts
+++ src/app/components/calendar-view/calendar-view-all/calendar-view-all.component.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/core';
 import { CalendarEvent, CalendarView, DAYS_OF_WEEK } from 'angular-calendar';
 import { colors } from '../calendar-utils/colors';
+import {addHours, startOfDay} from "date-fns";
 
 @Component({
   selector: 'mwl-demo-component',
@@ -25,7 +26,7 @@ export class CalendarViewAllComponent {
 
   events: CalendarEvent[] = [
     {
-      start: new Date('2022-06-23'),
+      start: new Date('2022-06-30'),
       end: new Date('2022-06-30'),
       title: 'One day excluded event',
       color: colors.red,
@@ -35,6 +36,65 @@ export class CalendarViewAllComponent {
       start: new Date('2022-06-23'),
       end: new Date('2022-06-30'),
       title: 'Multiple weeks event',
+      allDay: true,
+    },
+    {
+      start: new Date('2022-06-29'),
+      end: new Date('2022-06-29'),
+      title: 'One day excluded event',
+      color: colors.red,
+      allDay: true,
+    },
+    {
+      start: new Date('2022-07-01'),
+      end: new Date('2022-07-01'),
+      title: 'One day excluded event',
+      color: colors.red,
+      allDay: true,
+    },
+    {
+      start: addHours(startOfDay(new Date()), 10),
+      end: addHours(new Date(), 2),
+      title: 'A draggable event',
+      color: colors.yellow,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true,
+      },
+      draggable: true,
+    },{
+      start: addHours(startOfDay(new Date('2022-07-04')), 9),
+      end: addHours(new Date('2022-07-04'), 3),
+      title: 'A draggable event',
+      color: colors.blue,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true,
+      },
+      draggable: true,
+    },
+    {
+      start: addHours(startOfDay(new Date()), 11),
+      end: addHours(new Date(), 3),
+      title: 'A draggable event',
+      color: colors.yellow,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true,
+      },
+      draggable: true,
+    },
+    {
+      start: new Date('2022-07-06'),
+      end: new Date('2022-07-07'),
+      title: 'Multiple weeks event',
+      allDay: true,
+    },
+    {
+      start: new Date('2022-07-08'),
+      end: new Date('2022-07-08'),
+      title: 'One day excluded event',
+      color: colors.red,
       allDay: true,
     },
   ];
--- src/app/components/calendar-view/calendar-view-day/calendar-view-day.component.ts
+++ src/app/components/calendar-view/calendar-view-day/calendar-view-day.component.ts
@@ -5,6 +5,10 @@ import {
 } from '@angular/core';
 import { CalendarEvent, CalendarView, DAYS_OF_WEEK } from 'angular-calendar';
 import { colors } from '../calendar-utils/colors';
+import {
+  startOfDay,
+  addHours,
+} from 'date-fns';
 
 @Component({
   selector: 'app-calendar-view-day',
@@ -15,7 +19,7 @@ import { colors } from '../calendar-utils/colors';
 })
 export class CalendarViewDayComponent {
 
-  view: CalendarView = CalendarView.Month;
+  view: CalendarView = CalendarView.Day;
 
   viewDate: Date = new Date();
 
@@ -24,8 +28,71 @@ export class CalendarViewDayComponent {
 
   events: CalendarEvent[] = [
     {
-      start: new Date('2022-06-23'),
-      end: new Date('2022-06-30'),
+      start: new Date('2022-06-29'),
+      end: new Date('2022-06-29'),
+      title: 'One day excluded event',
+      color: colors.red,
+      allDay: true,
+    },
+    {
+      start: new Date('2022-07-01'),
+      end: new Date('2022-07-01'),
+      title: 'One day excluded event',
+      color: colors.red,
+      allDay: true,
+    },
+    {
+      start: addHours(startOfDay(new Date()), 10),
+      end: addHours(new Date(), 2),
+      title: 'A draggable event',
+      color: colors.yellow,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true,
+      },
+      draggable: true,
+    },{
+      start: addHours(startOfDay(new Date('2022-07-03')), 9),
+      end: addHours(new Date('2022-07-03'), 2),
+      title: 'A draggable event',
+      color: colors.blue,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true,
+      },
+      draggable: true,
+    },
+    {
+      start: addHours(startOfDay(new Date()), 11),
+      end: addHours(new Date(), 3),
+      title: 'A draggable event',
+      color: colors.blue,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true,
+      },
+      draggable: true,
+    }
+    ,{
+      start: addHours(startOfDay(new Date('2022-07-04')), 9),
+      end: addHours(new Date('2022-07-04'), 3),
+      title: 'A draggable event',
+      color: colors.blue,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true,
+      },
+      draggable: true,
+    },
+    {
+      start: new Date('2022-07-06'),
+      end: new Date('2022-07-07'),
+      title: 'Multiple weeks event',
+      allDay: true,
+    },
+    {
+      start: new Date('2022-07-08'),
+      end: new Date('2022-07-08'),
       title: 'One day excluded event',
       color: colors.red,
       allDay: true,
