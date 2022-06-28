f41e74969da22d24cf13e3415edef511a9f011e2
Flashacker <maxime3510@gmail.com>
2022-06-28 17:33:38 +0200

    Add student list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import {CalendarViewModule} from "./components/calendar-view/calendar-view.module";
+import { CalendarViewModule } from "./components/calendar-view/calendar-view.module";
 import { environment } from '../environments/environment';
 
 import { AngularSignaturePadModule } from '@almothafar/angular-signature-pad';
 import { SignaturePad } from './components/signature-pad/signature-pad.component';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { StudentList } from './components/student-list/student-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignaturePad,
+    StudentList
   ],
   imports: [
     CalendarViewModule,
@@ -24,6 +28,8 @@ import { ButtonModule } from 'primeng/button';
     BrowserAnimationsModule,
     AppRoutingModule,
     AngularSignaturePadModule,
+    TableModule,
+    DialogModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
