2c83cba41f5bdddc6d9d7089c20a63609decf118
solenn <solenn.provost@campus.academy>
2022-06-29 15:29:02 +0200

    event click

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {CalendarViewAllComponent} from "./components/calendar-view/calendar-view-all/calendar-view-all.component";
 import {CalendarViewDayComponent} from "./components/calendar-view/calendar-view-day/calendar-view-day.component";
 import {QrCodeImplComponent} from "./components/qr-code/qr-code-impl/qr-code-impl.component";
+import {StudentList} from "./components/student-list/student-list.component";
 
 const routes: Routes = [
   {path:'calendar', component:CalendarViewAllComponent},
   {path:'calendar-day', component:CalendarViewDayComponent},
   {path:'qrcode', component:QrCodeImplComponent},
+  {path:'student-list', component:StudentList},
 
 ];
 
