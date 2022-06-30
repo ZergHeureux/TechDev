2c30b03b2284578eac1b2d88936002ef3939270d
Yvon Lewis <yvonlewisghilbert@gmail.com>
2022-06-30 17:24:06 +0200

    gestion header en fonction de la route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { PrimeNGConfig } from 'primeng/api';
 
 import { ChangeDetectorRef, OnDestroy, Component, OnInit } from '@angular/core';
 import {MenuItem} from 'primeng/api';
+import { Router } from '@angular/router';
 import {MediaMatcher} from '@angular/cdk/layout';
 @Component({
   selector: 'app-root',
@@ -18,7 +19,7 @@ export class AppComponent implements OnInit {
   private _mobileQueryListener!: (() => void);
 
   constructor(private primengConfig: PrimeNGConfig,
-    changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,public router : Router) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
