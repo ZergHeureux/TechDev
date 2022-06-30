b8cd0b80b3b07e39d10f8e9dfca23f3c3bd860b4
solenn <solenn.provost@campus.academy>
2022-06-30 15:47:30 +0200

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import {LoginPageComponent} from "./components/login-page/login-page/login-page.
 import {StudentListPage} from "./pages/student-list/student-list.component";
 import {DashboardPage} from "./pages/dashboard/dashboard.component";
 import {CalendarPage} from "./pages/calendar/calendar.component";
+import {StudentListTeacherPage} from "./pages/student-list-teacher/student-list-teacher.component";
 
 const routes: Routes = [
   {path: '',   redirectTo: '/login', pathMatch: 'full' },
   {path:'login', component: LoginPageComponent},
   {path:'calendar', component:CalendarPage},
   {path:'dashboard', component:DashboardPage},
-  {path:'student-list', component:StudentListPage}
+  {path:'student-list', component:StudentListPage},
+  {path:'student-list-teacher', component:StudentListTeacherPage}
+
 ];
 
 @NgModule({
